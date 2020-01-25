d483b3f6d795c8217023871e5636f46743f1883f
ryan <whuk84@gmail.com>
2020-01-25 15:02:08 +0900

    add assertion in test

--- src/test/java/me/ryan/runwith/exec/MyTestClass.java
+++ src/test/java/me/ryan/runwith/exec/MyTestClass.java
@@ -6,6 +6,8 @@ import me.ryan.runwith.sample.Calculator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(RepeatRunner.class)
 public class MyTestClass {
 
@@ -15,14 +17,24 @@ public class MyTestClass {
     @Repeat(10) // 반복 횟수를 지정한다...
     public void testMyCode10Times() {
         //your test code goes here
-        System.out.println(calculator.add(5, 5));
+        int a = 5;
+        int b = 5;
+        int expected = 10;
+        int result = calculator.add(a, b);
+        System.out.println(result);
+        assertEquals(expected, result);
     }
 
     @Test
     @Repeat(5)  // 반복 횟수를 지정한다...
     public void testMyCode5Times() {
         //your test code goes here
-        System.out.println(calculator.add(2, 3));
+        int a = 2;
+        int b = 3;
+        int expected = 5;
+        int result = calculator.add(a, b);
+        System.out.println(result);
+        assertEquals(expected, result);
     }
 
 }
