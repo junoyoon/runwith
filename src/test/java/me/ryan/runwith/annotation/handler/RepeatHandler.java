53ca7d609a6fdd21d734c911589e5c52c1c9b736
ryan <whuk84@gmail.com>
2020-01-26 22:41:59 +0900

    [Need to params when repeat] add get params from repeat annotation. divide invoke method.

--- src/test/java/me/ryan/runwith/annotation/handler/RepeatHandler.java
+++ src/test/java/me/ryan/runwith/annotation/handler/RepeatHandler.java
@@ -2,6 +2,7 @@ package me.ryan.runwith.annotation.handler;
 
 
 import me.ryan.runwith.common.TestUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.runners.model.Statement;
@@ -14,19 +15,24 @@ public class RepeatHandler extends Statement {
     protected static final Log log = LogFactory.getLog(RepeatHandler.class);
 
     private final Statement next;
+    private final Object target;
     private final Method testMethod;
     private final int repeat;
     private final boolean repeatable;
 
-    public RepeatHandler(Statement next, Method testMethod) {
-        this(next, testMethod, TestUtils.getRepeatCount(testMethod), TestUtils.isRepeatable(testMethod));
+    private final String[] params;
+
+    public RepeatHandler(Statement next, Object target, Method testMethod) {
+        this(next, target, testMethod, TestUtils.getRepeatCount(testMethod), TestUtils.isRepeatable(testMethod));
     }
 
-    public RepeatHandler(Statement next, Method testMethod, int repeat, boolean repeatable) {
+    public RepeatHandler(Statement next, Object target, Method testMethod, int repeat, boolean repeatable) {
         this.next = next;
+        this.target = target;
         this.testMethod = testMethod;
         this.repeat = repeat;
         this.repeatable = repeatable;
+        this.params = TestUtils.getRepeatParams(testMethod);
     }
 
     @Override
@@ -40,8 +46,12 @@ public class RepeatHandler extends Statement {
             if (repeatable && log.isInfoEnabled()) {
                 log.info(String.format("Repeat # %d Method is [%s]", i + 1, this.testMethod.getName()));
             }
-            this.next.evaluate();
-
+            if (ArrayUtils.isNotEmpty(this.params)) {
+                Object[] paramArgs = TestUtils.getParamArgs(testMethod, this.params[i]);
+                testMethod.invoke(target, paramArgs);
+            } else {
+                this.next.evaluate();
+            }
             long elapsed = System.currentTimeMillis() - startTime;
             if (timeout > 0 && elapsed > timeout) {
                 throw new TimeoutException(
