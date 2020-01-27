1dfe5bf6d944837f08901684447c6dd6b529f0d9
ryan <whuk84@gmail.com>
2020-01-27 20:34:26 +0900

    add fast fibo

--- src/main/java/me/ryan/runwith/algo/Fibo.java
+++ src/main/java/me/ryan/runwith/algo/Fibo.java
@@ -5,7 +5,30 @@ public class Fibo {
         return this.fibo(number);
     }
 
-    public int fibo(int n) {
+    public int calculateFibo(int number, boolean isFast) {
+        if (isFast) {
+            return this.fastFibo(number);
+        }
+        return this.fibo(number);
+    }
+
+    private int fastFibo(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int[] arr = new int[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n];
+        }
+    }
+
+    private int fibo(int n) {
         if (n == 0) {
             return 0;
         } else if (n == 1) {
